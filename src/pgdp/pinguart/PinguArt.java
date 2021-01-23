51b03c7e178e6037797387c9fcc43ab9d455b826
Johannes Stöhr <johannes.stoehr@tum.de>
2021-01-23 12:44:34 +0100

    Not empty

--- src/pgdp/pinguart/PinguArt.java
+++ src/pgdp/pinguart/PinguArt.java
@@ -1,10 +1,14 @@
 package pgdp.pinguart;
 
-import static pgdp.MiniJava.*;
+import static pgdp.MiniJava.readInt;
+
+import java.util.stream.Stream;
 
 public class PinguArt {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        String inputMessage = "Bitte geben Sie eine Pingu-Art-Zahl ein:";
 
-	}
+        Stream.generate(() -> readInt(inputMessage)).limit(8).forEach(System.out::println);
+    }
 }
